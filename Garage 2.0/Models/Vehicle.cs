ced3be46379848207112a4d9411dae5d85f6ce7a
Mounir Darwich <mounirdarwich22@gmail.com>
2025-12-17 09:28:44 +0100

    Create Vehicle entity with required properties

---
+++ Garage 2.0/Models/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2._0.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string RegNumber { get; set; } = string.Empty;
+
+        [Required]
+        public VehicleType VehicleType { get; set; }
+
+        [StringLength(30)]
+        public string Color { get; set; } = string.Empty;
+
+        [StringLength(30)]
+        public string Brand { get; set; } = string.Empty;
+
+        [StringLength(30)]
+        public string Model { get; set; } = string.Empty;
+
+        [Range(0, 50)]
+        public int NumberOfWheels { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+    }
+}
---
+++ Garage 2.0/Models/VehicleType.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage_2._0.Models
+{
+    public enum VehicleType
+    {
+        Car,
+        Motorcycle,
+        Bus,
+        Truck,
+        Boat
+    }
+}
