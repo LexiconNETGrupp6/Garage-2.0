ae98638615ea8390502d3be6f54d7e9eb1b17038
Jacob <jackan202@gmail.com>
2025-12-17 11:42:31 +0100

    Added Seeded data and updayed database

--- Garage 2.0/Data/GarageContext.cs
+++ Garage 2.0/Data/GarageContext.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Garage_2._0.Models;
 
+namespace Garage_2._0.Data
+{
     public class GarageContext : DbContext
     {
-        public GarageContext (DbContextOptions<GarageContext> options)
+        public GarageContext(DbContextOptions<GarageContext> options)
             : base(options)
         {
         }
 
-        public DbSet<Garage_2._0.Models.Vehicle> Vehicle { get; set; } = default!;
+        public DbSet<Vehicle> Vehicle { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Vehicle>().HasData(
+                new Vehicle { Id = 1, RegNumber = "ABC123", VehicleType = VehicleType.Car, Color = "Red", Brand = "Volvo", Model = "V70", NumberOfWheels = 4 },
+                new Vehicle { Id = 2, RegNumber = "LGH436", VehicleType = VehicleType.Boat, Color = "Yellow", Brand = "East Marine", Model = "Viking Line", NumberOfWheels = 0 },
+                new Vehicle { Id = 3, RegNumber = "AHC745", VehicleType = VehicleType.Bus, Color = "Red", Brand = "Volvo", Model = "V7900", NumberOfWheels = 8 },
+                new Vehicle { Id = 4, RegNumber = "KAK156", VehicleType = VehicleType.Car, Color = "White", Brand = "Teesla", Model = "X", NumberOfWheels = 4 },
+                new Vehicle { Id = 5, RegNumber = "IKA71U", VehicleType = VehicleType.Truck, Color = "Blue", Brand = "Scania", Model = "G-series", NumberOfWheels = 6 },
+                new Vehicle { Id = 6, RegNumber = "ÅJAUIV", VehicleType = VehicleType.Motorcycle, Color = "Green", Brand = "Mercedez", Model = "L420", NumberOfWheels = 2 }
+            );
+        }
     }
+}
