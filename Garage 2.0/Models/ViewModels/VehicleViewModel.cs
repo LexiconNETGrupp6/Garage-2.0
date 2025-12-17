5b6efc8c3faa9cbe5775c621d6eebc0d4a16bd8a
Jacob <jackan202@gmail.com>
2025-12-17 10:32:25 +0100

    Added property firor ParkDuration and method for updating it

--- Garage 2.0/Models/ViewModels/VehicleViewModel.cs
+++ Garage 2.0/Models/ViewModels/VehicleViewModel.cs
@@ -1,9 +1,17 @@
-﻿namespace Garage_2._0.Models.ViewModels
+﻿using System.ComponentModel;
+
+namespace Garage_2._0.Models.ViewModels
 {
     public class VehicleViewModel
     {
         public VehicleType Type { get; set; }
         public string RegNumber { get; set; } = string.Empty;
         public DateTime ArrivalTime { get; set; }
+
+        [DisplayName("How long the vehicle has been parked")]
+        public TimeSpan ParkDuration { get; private set; }
+
+        public void UpdateParkDuration()        
+            => ParkDuration = DateTime.Now.Subtract(ArrivalTime);        
     }
 }
