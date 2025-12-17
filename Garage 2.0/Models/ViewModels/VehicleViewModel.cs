0c74167c75bf525e5269de4c4a5bc12873224e2b
Jacob <jackan202@gmail.com>
2025-12-17 10:23:46 +0100

    Created simple VehicleViewModel for the overview

---
+++ Garage 2.0/Models/ViewModels/VehicleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Garage_2._0.Models.ViewModels
+{
+    public class VehicleViewModel
+    {
+        public VehicleType Type { get; set; }
+        public string RegNumber { get; set; } = string.Empty;
+        public DateTime ArrivalTime { get; set; }
+    }
+}
