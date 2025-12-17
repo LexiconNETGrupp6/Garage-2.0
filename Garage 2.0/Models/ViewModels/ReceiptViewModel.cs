7d9e9340dc223f6c54e67086300bb6b21e28ea31
Jacob <jackan202@gmail.com>
2025-12-17 11:11:20 +0100

    ViewModel for the receipt

---
+++ Garage 2.0/Models/ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2._0.Models.ViewModels
+{
+    public class ReceiptViewModel
+    {
+        public Vehicle Vehicle { get; set; }
+
+        [DisplayName("Time at checkout")]
+        public DateTime CheckOutTime { get; set; }
+
+        [Range(5, 200)]
+        [DisplayName("Hourly price")]
+        public double PricePerHour { get; private set; } = 30;
+
+        [DisplayName("Whole parking duration")]
+        public TimeSpan ParkedDuration => CheckOutTime.Subtract(Vehicle.ArrivalTime);
+
+        [DisplayName("Total price for parking")]
+        public double TotalPrice => ParkedDuration.TotalHours * PricePerHour;
+    }
+}
