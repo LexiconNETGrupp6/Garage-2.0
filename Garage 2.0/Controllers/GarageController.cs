8b59629f277cd5888f7d2218ca00672b4d2f980b
Jacob <jackan202@gmail.com>
2025-12-17 09:18:19 +0100

    Created GarageController

---
+++ Garage 2.0/Controllers/GarageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Garage_2._0.Controllers
+{
+    public class GarageController : Controller
+    {        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
