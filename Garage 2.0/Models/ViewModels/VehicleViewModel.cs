00a2b06657471bd5ecbbff5c07952871390417c8
Jacob <jackan202@gmail.com>
2025-12-19 11:41:16 +0100

    Made overview (index) use the vehicle view model instead of vehicle

--- Garage 2.0/Models/ViewModels/VehicleViewModel.cs
+++ Garage 2.0/Models/ViewModels/VehicleViewModel.cs
@@ -4,14 +4,20 @@ namespace Garage_2._0.Models.ViewModels
 {
     public class VehicleViewModel
     {
-        public VehicleType Type { get; set; }
+        public int Id { get; set; }
+        public VehicleType VehicleType { get; set; }
         public string RegNumber { get; set; } = string.Empty;
         public DateTime ArrivalTime { get; set; }
 
         [DisplayName("How long the vehicle has been parked")]
-        public TimeSpan ParkDuration { get; private set; }
+        public String ParkDuration { get; private set; } = string.Empty;
 
-        public void UpdateParkDuration()        
-            => ParkDuration = DateTime.Now.Subtract(ArrivalTime);        
+        public void UpdateParkDuration()
+        {
+            var duration = DateTime.Now - ArrivalTime;
+            var days = (int)duration.TotalDays;
+            var timePart = duration.ToString(@"hh\:mm\:ss");
+            ParkDuration = (days > 0 ? $"{days}d " : "") + timePart;
+        }            
     }
 }
