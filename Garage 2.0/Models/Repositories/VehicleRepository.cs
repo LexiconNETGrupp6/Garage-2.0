5a96ed39d2603606ff97c3a124bcfdd678dec1df
Jacob <jackan202@gmail.com>
2025-12-22 10:20:36 +0100

    Implmented repository containing all used methods so far

---
+++ Garage 2.0/Models/Repositories/VehicleRepository.cs
@@ -0,0 +1,45 @@
+﻿using Garage_2._0.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Garage_2._0.Models.Repositories
+{
+    public class VehicleRepository
+    {
+        private readonly GarageContext _context;
+
+        public IEnumerable<Vehicle> Vehicles => _context.Vehicle;
+
+        public VehicleRepository(GarageContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Vehicle vehicle)
+        {
+            _context.Add(vehicle);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Vehicle vehicle)
+        {
+            _context.Update(vehicle);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Remove(Vehicle vehicle)
+        {
+            _context.Vehicle.Remove(vehicle);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<Vehicle, bool>> predicate, CancellationToken token = default) 
+            => await _context.Vehicle.AnyAsync(predicate, token);
+
+        public async Task<Vehicle?> FirstOrDefaultAsync(Expression<Func<Vehicle, bool>> predicate, CancellationToken token = default) =>
+            await _context.Vehicle.FirstOrDefaultAsync(predicate, token);
+
+        public async Task<Vehicle?> FindAsync(Expression<Func<Vehicle, bool>> predicate, CancellationToken token = default) =>
+            await _context.Vehicle.FirstOrDefaultAsync(predicate, token);
+    }
+}
